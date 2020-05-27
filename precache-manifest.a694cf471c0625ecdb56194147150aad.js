acd9c804f2f56d67a7eeee76042e031f9fcbaa04
James Carlo Luchavez <mrstudyfirst@gmail.com>
2020-05-26 19:48:28 -0700

    Updates

---
+++ precache-manifest.a694cf471c0625ecdb56194147150aad.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6b538c30966571cc42ae651e2b899b",
+    "url": "/jarcalc_front/index.html"
+  },
+  {
+    "revision": "e135bdb0b8b8152b78c4",
+    "url": "/jarcalc_front/static/css/2.149bdb54.chunk.css"
+  },
+  {
+    "revision": "e135bdb0b8b8152b78c4",
+    "url": "/jarcalc_front/static/js/2.cd8ebd2a.chunk.js"
+  },
+  {
+    "revision": "282f6f64b9f3c198ffc9defe1ee4ccb7",
+    "url": "/jarcalc_front/static/js/2.cd8ebd2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eadf029b75afa527d80a",
+    "url": "/jarcalc_front/static/js/main.9a9b5deb.chunk.js"
+  },
+  {
+    "revision": "86f1ef2891f6a2cca38b",
+    "url": "/jarcalc_front/static/js/runtime-main.ec43b2ba.js"
+  },
+  {
+    "revision": "e101c42e43fe2c8edcde3d8cf52b193a",
+    "url": "/jarcalc_front/static/media/JAR.e101c42e.png"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/jarcalc_front/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/jarcalc_front/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/jarcalc_front/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/jarcalc_front/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/jarcalc_front/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/jarcalc_front/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/jarcalc_front/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/jarcalc_front/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/jarcalc_front/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/jarcalc_front/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/jarcalc_front/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/jarcalc_front/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/jarcalc_front/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/jarcalc_front/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/jarcalc_front/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/jarcalc_front/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/jarcalc_front/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/jarcalc_front/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/jarcalc_front/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/jarcalc_front/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/jarcalc_front/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/jarcalc_front/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/jarcalc_front/static/media/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "4a28c895ffe3bd184490cada61bb749c",
+    "url": "/jarcalc_front/static/media/fa-brands-400.4a28c895.eot"
+  },
+  {
+    "revision": "77ace0fee45b138f023459bf3af34ba0",
+    "url": "/jarcalc_front/static/media/fa-brands-400.77ace0fe.ttf"
+  },
+  {
+    "revision": "82927ee13ebdbbb7ae72dc00b86624ce",
+    "url": "/jarcalc_front/static/media/fa-brands-400.82927ee1.svg"
+  },
+  {
+    "revision": "a0f6b35c2b55babc93cb9b15791a1d4c",
+    "url": "/jarcalc_front/static/media/fa-brands-400.a0f6b35c.woff2"
+  },
+  {
+    "revision": "d3a718bc1772e114ece0c2acbda89747",
+    "url": "/jarcalc_front/static/media/fa-brands-400.d3a718bc.woff"
+  },
+  {
+    "revision": "071dad85141e5ae94ee3fddb79a589ae",
+    "url": "/jarcalc_front/static/media/fa-duotone-900.071dad85.woff2"
+  },
+  {
+    "revision": "4486ac29725c9135ac6313f567906a83",
+    "url": "/jarcalc_front/static/media/fa-duotone-900.4486ac29.eot"
+  },
+  {
+    "revision": "52c4adbc17713d632194ed2359b15505",
+    "url": "/jarcalc_front/static/media/fa-duotone-900.52c4adbc.woff"
+  },
+  {
+    "revision": "5bafbe652c040ec958005a1a255dddf0",
+    "url": "/jarcalc_front/static/media/fa-duotone-900.5bafbe65.svg"
+  },
+  {
+    "revision": "cf3b376d9576e4a9b0ba7ee4cdb3ec47",
+    "url": "/jarcalc_front/static/media/fa-duotone-900.cf3b376d.ttf"
+  },
+  {
+    "revision": "1980e02850f8d6c2e02280a60674f547",
+    "url": "/jarcalc_front/static/media/fa-light-300.1980e028.eot"
+  },
+  {
+    "revision": "4433b4f4806f7bfb430596701dd4fde8",
+    "url": "/jarcalc_front/static/media/fa-light-300.4433b4f4.svg"
+  },
+  {
+    "revision": "5bdaa8582fd409b4a3fd9f03916de415",
+    "url": "/jarcalc_front/static/media/fa-light-300.5bdaa858.ttf"
+  },
+  {
+    "revision": "68c2f74f76d4236722607d6eeb8b105d",
+    "url": "/jarcalc_front/static/media/fa-light-300.68c2f74f.woff2"
+  },
+  {
+    "revision": "f056755c8cd097b5f5ffde3e98387631",
+    "url": "/jarcalc_front/static/media/fa-light-300.f056755c.woff"
+  },
+  {
+    "revision": "5739d02394088b39be41cbe628723cfa",
+    "url": "/jarcalc_front/static/media/fa-regular-400.5739d023.eot"
+  },
+  {
+    "revision": "606e54f607334e45061aab955bbabe29",
+    "url": "/jarcalc_front/static/media/fa-regular-400.606e54f6.svg"
+  },
+  {
+    "revision": "6ee0bdf1a4b4aad88663dfb01ef6f789",
+    "url": "/jarcalc_front/static/media/fa-regular-400.6ee0bdf1.ttf"
+  },
+  {
+    "revision": "dd049a6496eea2a18937237b999ea8fa",
+    "url": "/jarcalc_front/static/media/fa-regular-400.dd049a64.woff2"
+  },
+  {
+    "revision": "f5c3ba7919c746a90974c94d90e502e1",
+    "url": "/jarcalc_front/static/media/fa-regular-400.f5c3ba79.woff"
+  },
+  {
+    "revision": "0f7c8f7d7cd80287a094702ebaf8019c",
+    "url": "/jarcalc_front/static/media/fa-solid-900.0f7c8f7d.woff"
+  },
+  {
+    "revision": "16e9dbeb2afd22d5cf0e7eeb2b2879ae",
+    "url": "/jarcalc_front/static/media/fa-solid-900.16e9dbeb.woff2"
+  },
+  {
+    "revision": "977e6fae30d6f3f1aed9997f1928f70d",
+    "url": "/jarcalc_front/static/media/fa-solid-900.977e6fae.ttf"
+  },
+  {
+    "revision": "98d38a5db49c92a68bd3b83b906ee2c9",
+    "url": "/jarcalc_front/static/media/fa-solid-900.98d38a5d.eot"
+  },
+  {
+    "revision": "ff4da5ed81cdfe2b2e68d6dd9978d96e",
+    "url": "/jarcalc_front/static/media/fa-solid-900.ff4da5ed.svg"
+  }
+]);
